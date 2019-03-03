e7118e5df81e62d907b7958ef3b79a1f436703ba
Supphachoke Suntiwichaya <mrchoke@gmail.com>
2019-03-03 19:55:05 +0700

    fixed docker restart issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -m
 
-unitd --no-daemon --control unix:/var/run/control.unit.sock &
-
+unitd --no-daemon --control '*:8000'  --pid /var/run/unitd.pid &
 if [ ! -f /started ];  then
 
 cd /srv/app
@@ -10,7 +9,8 @@ cd /srv/app
 npm install
 npm link unit-http
 
-curl -X PUT -d @/srv/app/unit.json   --unix-socket /var/run/control.unit.sock  http://localhost/config/
+curl -X PUT -d @/srv/app/unit.json  http://localhost:8000/config/
+curl -X PUT -d '"/dev/stdout"' http://localhost:8000/config/access_log
 
 touch /started
 
